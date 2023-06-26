1124463ac9cd2fe5471b5b06702565b7d6541bf6
Nicolas Sousa <nicollassouza141@gmail.com>
2023-06-26 12:50:41 -0300

    Introdução ao TypeScript

---
+++ src/teste.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(2, 3));
+const nome = "Nicolas";
+console.log(saudacao(nome));
---
+++ src/teste.ts
@@ -0,0 +1,12 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(2, 3));
+
+const nome = "Nicolas";
+console.log(saudacao(nome));
